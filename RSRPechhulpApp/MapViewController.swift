2501c56adf43b1e92ca9a20522d6e664520e949c
taraere <taraelsen@gmail.com>
2017-11-05 19:37:12 +0100

    MapView and LocationManager

---
+++ RSRPechhulpApp/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  RSRPechhulpApp
+//
+//  Created by Tara Elsen on 05/11/2017.
+//  Copyright © 2017 Tara Elsen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController : UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView?
+    
+}
