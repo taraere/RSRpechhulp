0ed3f85d0b4c5166fae7f09fa3a70c321714cd54
taraere <taraelsen@gmail.com>
2017-11-05 13:06:38 +0100

    Created splash screen and start screen

--- RSRPechhulpApp/StartViewController.swift
+++ RSRPechhulpApp/StartViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  RSRPechhulpApp
 //
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
