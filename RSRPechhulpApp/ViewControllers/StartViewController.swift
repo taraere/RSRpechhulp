c7c83b1bc9fd1619fe702f956744ecf27ef1bd3f
taraere <taraelsen@gmail.com>
2017-12-09 16:30:51 +0100

    alter buttons slightly

--- RSRPechhulpApp/ViewControllers/StartViewController.swift
+++ RSRPechhulpApp/ViewControllers/StartViewController.swift
@@ -14,6 +14,8 @@ class StartViewController: UIViewController {
     @IBOutlet weak var barButtonItem: UIBarButtonItem!
     @IBOutlet weak var topConstraint: NSLayoutConstraint!
     @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var height2Constraint: NSLayoutConstraint!
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +27,8 @@ class StartViewController: UIViewController {
             barButtonItem.title = nil
             infoButton.isHidden = false
             widthConstraint.constant = -250
+            heightConstraint.constant = -20
+            height2Constraint.constant = -20
             topConstraint.constant = 60
         } else {
             infoButton.isHidden = true
